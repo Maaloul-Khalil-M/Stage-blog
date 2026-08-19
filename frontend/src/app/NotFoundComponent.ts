c9d99e61bf61fbdc6b12047f9890e318f7ae8602
Maaloul Mohamed Khalil <maaloul.mohamedkhalil@gmail.com>
2026-08-19 15:04:18 +0100

    intro to material 1

---
+++ frontend/src/app/NotFoundComponent.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+             selector: 'app-notfound',
+             template: '404 sa7bi',
+           })
+export class NotFoundComponent {
+}
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,12 @@
 import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideEchartsCore} from 'ngx-echarts';
 import * as echarts from 'echarts';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
     provideEchartsCore({echarts}),
     provideBrowserGlobalErrorListeners()
   ]
