e7a87f883be8047298dac77cb558f6261f9687bc
Maaloul Mohamed Khalil <maaloul.mohamedkhalil@gmail.com>
2026-08-19 13:39:54 +0100

    add front libs

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {provideEchartsCore} from 'ngx-echarts';
+import * as echarts from 'echarts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    
+    provideEchartsCore({echarts}),
+    provideBrowserGlobalErrorListeners()
   ]
 };
